4983dd833877bd93e79161aa5fa9c9b6d5f5d2f8
Lonzales <miguelcecena3b@gmail.com>
2026-06-26 19:54:55 -0700

    Se agregaron estilos y la base de la aplicación en cuanto a diseño.

--- js/Script.js
+++ js/Script.js
@@ -1,26 +1,10 @@
-let testPhpUrl = 'php/test.php';
-
-const generarAlerta = (texto) => {
-    alert(texto);
+const evaluarRotacion = () => {
+    $('#sideMenuBtn i').toggleClass('rotateArrow')
 }
 
-const generarEtiqueta = (texto) => {
-    $('body').append(`${texto}`);
-}
+$('#sideMenuBtn').on('click', () => {
+    $('#container').toggleClass('moveLeft');
+    $('#sideContainer').toggleClass('moveRight');
 
-$('button').on('click', function () {
-    $.ajax({
-        url: testPhpUrl,
-        method: 'POST',
-        dataType: 'json',
-        data: { accion: $(this).val() },
-        success: (resp) => {
-            console.log(resp.resultado);
-
-            if (resp.accion === 'alerta')
-                generarAlerta(resp.resultado);
-            else if (resp.accion === 'etiqueta')
-                generarEtiqueta(resp.resultado);
-        }
-    })
+    evaluarRotacion();
 });
